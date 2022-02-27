0cd9451e00c89da41d3bc6ba19bb8d7fc2df9959
ducanh0910 <61743201+Ducanh0910@users.noreply.github.com>
2022-02-27 19:09:50 +0700

    Update processedDetail.blade.php

--- resources/views/admin/orders/processedDetail.blade.php
+++ resources/views/admin/orders/processedDetail.blade.php
@@ -99,13 +99,13 @@
                                 </div>
 
 
-                                @foreach ($order->orderDetail as $item)
+
                                 <div class="alert alert-primary" role="alert" align='right'>
                                     <button onclick="return complete()" class="btn btn-success btn_print" type="submit" role="button"><i class="fa fa-pencil" aria-hidden="true"></i>In sao kê</button>
 
-                                    <a href="/admin/orders/{{$item->id}}/processed-detail" class="btn btn-warning">Giao hàng</a>
+                                    <a href="/admin/orders/{{$order->id}}/processed-detail" class="btn btn-warning">Giao hàng</a>
 								</div>
-                                @endforeach
+
 							</div>
                         </div>
 
